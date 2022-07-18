68630940d8fc964e88a86d9c2cb8576a3851df4b
Alvin Accad <alvinaccad5@gmail.com>
2022-07-18 09:01:07 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,6 +15,25 @@
 
 // a) Create a test with expect statements using the variables provided.
 
+describe("codedMessage", () => {
+    const secretCodeWord1 = "Lackadaisical"
+    // Expected output: "L4ck4d41s1c4l"
+    const result = "L4ck4d41s1c4l"
+    const secretCodeWord2 = "Gobbledygook"
+    // Expected output: "G0bbl3dyg00k"
+    const secretCodeWord3 = "Eccentric"
+    // Expected output: "3cc3ntr1c"
+    
+    it("takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.", () => {
+        expect(codedMessage(secretCodeWord1)).toEqual(result)
+        expect(codedMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect(codedMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
+
+//     ReferenceError: codedMessage is not defined
+
+
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
 const secretCodeWord2 = "Gobbledygook"
@@ -25,10 +44,63 @@ const secretCodeWord3 = "Eccentric"
 
 // b) Create the function that makes the test pass.
 
+// Pseudocode:
+// 1. Create a function and a parameter to take a string input
+// 2. We will want to use the .replaceAll built-in method, because it will replace multiple occurances of the letter we are trying to replace. If we used .replace only, it would just change the first occurance of the letter we want to change.
+//      2a. Create a new variable called aString. Set it to equal the string parameter and use the .replaceAll method. We want to take the letter "a" and replace it with the string "4"
+//      2b. Next, we will create a new variable called eString and set it to aString, the variable we just changed previously. We then use the same .replaceAll built-in method and replace "e" to "3".
+//      2c. We continue steps 2b for the strings "i" and "o"
+// 3. Once we have all 4 letters that we want to replaceAll with, then we are finished and all we have to do is return the last variable we had created, because it has stored all the changes we had made.
+
+const codedMessage = (string) => {
+
+    let aString = string.replaceAll("a", "4")
+    let eString = aString.replaceAll("e", "3")
+    let iString = eString.replaceAll("i", "1")
+    let oString = iString.replaceAll("o", "0")
+    let aString1 = oString.replaceAll("A", "4")
+    let eString1 = aString1.replaceAll("E", "3")
+    let iString1 = eString1.replaceAll("I", "1")
+    let oString1 = iString1.replaceAll("O", "0")
+
+    return oString1
+
+}
+
+console.log(codedMessage(secretCodeWord1)) // Output: L4ck4d41s1c4l
+console.log(codedMessage(secretCodeWord2)) // Output: G0bbl3dyg00k
+console.log(codedMessage(secretCodeWord3)) // Output: 3cc3ntr1c
+
+//  PASS  ./code-challenges.test.js
+//   codedMessage
+//   ✓ takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0. (1 ms)
+
+
+// Some notes I would add to this question is that uppercase characters were throwing off the output. An example was "Eccentric" would not change the E to a 3, because it was capitalized. I tried using a .LowerCase method, but realized it would change "Lackadaisical" to "l4ck4d41s1c4l"...which would make it incorrect, because the L was changed to lowercase. The final function works as intended, but it's very sloppy to read and could be coded more optimally.
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
+
+describe("returnArrayWithWords", () => {
+    const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+
+    const letterA = "a"
+    // Expected output: ["Mango", "Apricot", "Peach"]
+    const output1 = ["Mango", "Apricot", "Peach"]
+
+    const letterE = "e"
+    // Expected output: ["Cherry", "Blueberry", "Peach"]
+    const output2 = ["Cherry", "Blueberry", "Peach"]
+        
+    it("takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.", () => {
+        expect(returnArrayWithWords(fruitArray, letterA)).toEqual(output1)
+        expect(returnArrayWithWords(fruitArray, letterE)).toEqual(output2)
+    })
+})
+
+//     ReferenceError: returnArrayWithWords is not defined
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -40,10 +112,57 @@ const letterE = "e"
 
 // b) Create the function that makes the test pass.
 
+// Psuedocode:
+// 1. Create a function called returnArrayWithWords and use 2 parameters called array and letter
+// 2. Create a newArray variable that holds an empty array. This is what we will want to return in the end
+// 3. Use a for loop to iterate through each item in the array
+// 4. Create an if conditional
+//      4a. We want to check if the array of [i] includes the letter from the parameter. We will use the .includes built-in method.
+//      4b. We also want to use a .toLowerCase built-in method, because we want to check the characters from the beginning of the word if it contains the parameter from letter
+//      4c. Within the if statement, if the word does include the letter, then we want to do a .push on the array of i that we are on into the newArray. This will essentially put the word in the new array if the letter is in the word.
+// 5. Return the new array.
+
+const returnArrayWithWords = (array, letter) => {
+    let newArray = []
+
+    for(let i=0; i<array.length; i++) {
+        if(array[i].toLowerCase().includes(letter)) {
+            newArray.push(array[i])
+        }
+    
+    }
+    return newArray;
+}
+
+// console.log(returnArrayWithWords(fruitArray, letterA))
+// console.log(returnArrayWithWords(fruitArray, letterE))
+
+// PASS  ./code-challenges.test.js
+// returnArrayWithWords
+//   ✓ takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0. (1 ms)
+
+
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+
+describe("fullHouse", () => {
+    const hand1 = [5, 5, 5, 3, 3]
+    // Expected output: true
+    const hand2 = [5, 5, 3, 3, 4]
+    // Expected output: false
+    const hand3 = [5, 5, 5, 5, 4]
+    // Expected output: false
+            
+    it("takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.", () => {
+        expect(fullHouse(hand1)).toEqual(true)
+        expect(fullHouse(hand2)).toEqual(false)
+        expect(fullHouse(hand3)).toEqual(false)
+    })
+})
+
+//     ReferenceError: fullHouse is not defined
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -52,5 +171,71 @@ const hand2 = [5, 5, 3, 3, 4]
 const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
+const hand4 = [1, 2, 1, 2, 1] // Return true
+const hand5 = [0, 0, 1, 1, 1] // Return true
+const hand6 = [1, 2, 3, 4, 5] // Return false
+
 
 // b) Create the function that makes the test pass.
+
+
+// This was hands down, the hardest problem I had ever seen on an assessment. After about 2 hrs~ of failing the problem (wrong answers/ideas below), I had taken a long break from looking at it and came back with a refreshed mind and solved this with a solid solution within ~15 mins of trying again. I ran additional arrays to ensure the code was working with numbers in different positions and it still worked.
+
+// Psuedocode:
+// 1. Create a function called fullHouse and it takes in an array
+// 2. Use the array and set it to array.sort to sort the array from lowest to highest
+// 3. Use an if condition to check if array position 0, 1, & 2 is equal to each other and 3 and 4 is equal to each other. If the condition is met, then return true.
+// 4. Use an else if condition to check if array position 0 and 1 is equal to each other and 2, 3, & 4 is equal to each other. If the condition is met, then return true.
+// 5. There are no other conditions that can be true, so return false.
+
+
+const fullHouse = (array) => {
+    array = array.sort((a, b) => a-b)
+    if((array[0] === array[1] && array[0] === array[2] && array[1] === array[2]) && (array[3] === array[4])) {
+        return true;
+    } else if((array[0] === array[1]) && (array[2] === array[3] && array[2] === array[4] && array[3] === array[4])) {
+        return true;
+    }
+    return false;
+}
+
+console.log(fullHouse(hand1)) // Output: true
+console.log(fullHouse(hand2)) // Output: false
+console.log(fullHouse(hand3)) // Output: false
+
+console.log(fullHouse(hand4)) // Output: true
+console.log(fullHouse(hand5)) // Output: true
+console.log(fullHouse(hand6)) // Output: false
+
+// PASS  ./code-challenges.test.js
+// fullHouse
+//   ✓ takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind. (2 ms)
+
+
+// const fullHouse = (array) => {
+//     let count = 0
+//     let countTwo = 0
+
+//     // for(let i=0; i<array.length; i++) {
+//     //     for(let j=i+1; j<array.length; j++) {
+//     //         if(array[i] === array[j] && count < 4) {
+//     //             count++;
+//     //         } else {
+//     //             countTwo++;
+//     //         }
+//     //     }
+//     //     return count + " " + countTwo
+//     // }
+// }
+
+
+
+// if(array[0] === array[1] || array[0] === array[2] || array[0] === array[3] || array[0] === array[4]) {
+//     count++
+// } else if(array[1] === array[2] || array[1] === array[3] || array[1] === array[4]) {
+//     count++
+// } else if (array[2] === array[3] || array[2] === array[4]) {
+//     count++
+// } else if((array[3] === array[4])) {
+//     count++
+// }
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a hash in Ruby?
 
-Your answer:
+Your answer: A hash in Ruby is a key:value pair. Hashes are common in hashmaps and JSON data. A key is a unique data that only 1 of it can exist. There can be multiple values, but only 1 hash.
 
-Researched answer:
+Researched answer: A hash in Ruby is a data structure. Hashes hold data using key-value relationship. Hashes are created using symbols as keys and data types as values. Hashes can be represented like a dictionary that each keys are letters in an alphabet and easily obtainable by looking it up. Keys are the letters in the alphabet and values are the actual words in the dictionary.
 
 2. What is a gem?
 
-Your answer:
+Your answer: A gem is used in Ruby as a library to allow developers access to different functinality to their codebase. Gems make it easier for developers to use these pre-built libraries so they are not re-inventing the wheel. An example of a Ruby gem is Rspec, which is used for testing Ruby code.
 
-Researched answer:
+Researched answer: Gems in Rails are libraries that allow users to spend less time coding to accomplish different functionality within their code. Gems contain libraries that are reusable to users, so developers do not need to duplicate the same code to accomplish the same task as other developers. A popular gem for Ruby on Rails is RSpec. Rspec is a testing gem to allow developers to test their code and ensure it is working properly.
 
 3. What is Ruby on Rails?
 
-Your answer:
+Your answer: Ruby is a software developer language. Rails is a framework that works with Ruby. Putting them together allow a large portion of a full-stack application to be put together. Ruby on Rails is also really good for web pages and creating websites. Ruby on Rails makes it easy to build the front-end and the back-end.
 
-Researched answer:
-
+Researched answer: Ruby on Rails is an open-source web application development framework used in the Ruby language. Using Ruby on Rails makes it easy for developers, because of everything that is pre-built in it to quickly make a web application. Ruby on Rails uses MVC architecture (Model, View, and Controller). RoR uses the model and controller for the backend and the view for the frontend.
+s
 4. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A database holds the data using columns and rows. Relational databases uses commands like SELECT, FROM, ORDER BY, LIMIT, etc. Examples of relational databases are PostgreSQL and MySQL. There are also non-relational databases, which are different than relational databases. 
 
 Researched answer:
 
 5. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: Primary keys are used in databases and are the unique and only one of each primary key can exist. Each value in a database is unique by it's primary key. For example, if there is a database of millions of Facebook accounts, then it would be difficult to distinguish which belongs to who. So, a primary key is assigned to each account and allows databases to filter through each unique id.
 
-Researched answer:
+Researched answer: A primary key is also known as primary keyword. Primary keys are unique identifiers in a relational database and can only exist one primary key. An example of a primary key is when a customer shops at a store and the customer can shop in the store multiple times. However, a primary key can be placed on the specific order at the specific time the customer came in. So if a user went to the store and bought a t-shirt one time and a pair of shoes next time, then each visit would have a primary key and unique to that specific purchase at the time. Primary keys are important, because it is the sole purpose of a relational database. Each table within a database needs primary keys to keep track and maintain unique records.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: RESTful routes uses HTTP verbs (get, post, put, delete, patch) to the controller for CRUD actions (Create, Read, Update, Delete). RESTful routes are used to make it easier to navigate in an application using consistent URL paths.
 
-2. JSON:
+2. JSON: JSON stands for Javascript Object notation. JSON is text-based data that is based on key-value pairs and ordered lists. JSON is popular with many databases and relational databases like PostgreSQL and MySQL has native support.
 
-3. ERB:
+3. ERB: ERB stands for Embedded Ruby. It is an HTML file that Rails will communicate with Ruby to add content to the file and output an HTML file.
 
-4. Params:
+4. Params: Params is a parameter which is passed to the controller through GET or POST requests.
 
-5. API:
+5. API: API stands for Application Programming Interface. It allows different software to communicate with each other. An API software allows use of data and software.
